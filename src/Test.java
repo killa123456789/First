d1319caa37dd6bad97016b3c679f209f5dcb2926
IIS <ujh@jhfgjh>
2016-04-07 13:44:13 +0300

    sout+

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,9 @@
  * Created by IIS on 07.04.2016.
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello");
+        System.out.println("azaazazazaz");
     }
 }
